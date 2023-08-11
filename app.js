a2a4d8315cc7c169d0914d23ba9c5cb2d908919a
Pannudeepak <118485653+Pannudeepak@users.noreply.github.com>
2023-08-11 18:23:13 -0500

    added footer and changes in logo, adition of grids

--- app.js
+++ app.js
@@ -1,52 +1,38 @@
 const express = require("express");
 const session = require("express-session");
+//const fetch = require('node-fetch');
 const path = require("path");
 const router = express.Router();
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("/public"));
+app.use(express.static("public"));
 app.set("view engine", "pug");
 
 app.use(
-    session({
-        saveUninitialized: true,
-        secret: 'deepak',
-        resave: true,
-        saveUninitialized: false,
-        cookie: {
-            path: "/",
-            maxAge: 3600000
-        }
-    })
+  session({
+    saveUninitialized: true,
+    secret: "deepak",
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      path: "/",
+      maxAge: 3600000,
+    },
+  })
 );
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-
-
-    app.get("/",(req, res) => {
-        res.render("home", { title: "Home" });
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('*', function (req, res) {
-    res.status(404).render("error/404", { url: req.url, message: "Page not found" });
+app.get("*", function (req, res) {
+  res
+    .status(404)
+    .render("error/404", { url: req.url, message: "Page not found" });
 });
 
 app.listen(port, () => {
-    console.log(`app is listening on port ${port}`);
+  console.log(`app is listening on port ${port}`);
 });
