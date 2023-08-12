f75a1cef8eca1e986854415b152cffdb1393a026
Pannudeepak <118485653+Pannudeepak@users.noreply.github.com>
2023-08-11 21:46:57 -0500

    changed port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const session = require("express-session");
 const path = require("path");
 const router = express.Router();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
